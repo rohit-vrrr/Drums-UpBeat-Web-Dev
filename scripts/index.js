7856e838503d4056d7c623e35894eef5e5d15772
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-05 14:04:55 +0530

    Added onclick event

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-alert("Hello!");
+// for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//   document.querySelectorAll(".drum")[i].addEventListener("click", () => alert("Click!"));
+// }
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].onclick = () => {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  }
+}
