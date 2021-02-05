f08a50e177dd81095a37c4487887e3575b076922
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-05 16:43:04 +0530

    Added key press events

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,45 @@
 // for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-//   document.querySelectorAll(".drum")[i].addEventListener("click", () => alert("Click!"));
+//   document.querySelectorAll(".drum")[i].onclick = () => alert("Click!")
 // }
 
+/*Button Press*/
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].onclick = () => {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() { makeSound(this.innerHTML) });
+}
+
+/*Keyboard Press*/
+document.addEventListener("keydown", function(event) { makeSound(event.key) });
+
+/*Playing sounds*/
+function makeSound(char) {
+  switch (char) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default: console.log(char);
   }
 }
